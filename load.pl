9d82f2af805afc1555122b28bf19212c6ead357b
Wouter Beek <me@wouterbeek.com>
2013-11-13 07:10:44 +0100

    Added Beekeeper subproject and added dotCloud.

--- load.pl
+++ load.pl
@@ -8,7 +8,21 @@ user:project_name('PraSem').
 load_prasem:-
   % PraSem
   use_module(prasem(prasem)),
-  
+gtrace,
+  % Beekeeper
+  absolute_file_name(
+    prasem('Beekeeper'),
+    BK_Dir,
+    [access(read),file_type(directory)]
+  ),
+  absolute_file_name(
+    load,
+    BK_File,
+    [access(read),file_type(prolog),relative_to(BK_Dir)]
+  ),
+  ensure_loaded(BK_File),
+  load_bk(BK_Dir),
+
   % EnergyLabels
   absolute_file_name(
     prasem('EnergyLabels'),
