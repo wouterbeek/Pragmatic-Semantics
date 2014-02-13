012b81c6e70a2200df5ccd4e12dd7e2aae92cbc7
Wouter Beek <me@wouterbeek.com>
2014-02-13 11:42:09 +0100

    Upped PGC and blobs test case.

--- Tests/write_blobs.pl
+++ Tests/write_blobs.pl
@@ -1,11 +1,12 @@
 :- initialization(test).
 
 portray(Term):-
-  blob(Term, stream), !,
+  blob(Term, Type),
+  Type \== text, !,
   write(monkey).
 
 test:-
   open('README.md', read, Stream),
-  write_term(Stream, [blob(portray)]),
+  write_term(Stream, [blobs(portray)]),
   nl,
   halt.
