5dcc172924529c88a6a5a09511fdbf561746483f
Wouter Beek <me@wouterbeek.com>
2014-04-04 15:35:49 +0200

    Removed test cases for arithmetic functions.

--- Tests/test_arithmetic_0.pl
+++
@@ -1,7 +0,0 @@
-:- use_module(library(arithmetic)).
-
-:- arithmetic_function(xsd_div/2).
-
-xsd_div(X, Y, Z):-
-  Z is floor(X / Y).
-
--- Tests/test_arithmetic_1.pl
+++
@@ -1,9 +0,0 @@
-:- module(test_arithmetic1, []).
-
-:- use_module(library(arithmetic)).
-
-:- arithmetic_function(xsd_div/2).
-
-xsd_div(X, Y, Z):-
-  Z is floor(X / Y).
-
