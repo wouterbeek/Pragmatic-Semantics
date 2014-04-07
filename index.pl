211ab2f90b76ea3044a0142616e8a76c1e8bdaee
Wouter Beek <me@wouterbeek.com>
2014-04-07 13:42:59 +0200

    Updated index.

--- index.pl
+++ index.pl
@@ -1,19 +1,17 @@
 % Index for the Pragmatic Semantics repository.
 
-pl_repository_index(Dir):-
-  % Importing project directory.
-  assert(user:file_search_path(project, Dir)),
-  
-  assert(user:file_search_path(bk,     project(beekeeper      ))),
-  assert(user:file_search_path(ckan,   project(ckan           ))),
-  assert(user:file_search_path(dh,     project(data_hives     ))),
-  assert(user:file_search_path(el  ,   project(energy_labels  ))),
-  assert(user:file_search_path(humr,   project(humr           ))),
-  assert(user:file_search_path(id,     project(ideaology      ))),
-  assert(user:file_search_path(iotw,   project(iotw           ))),
-  assert(user:file_search_path(lodobs, project(lod_observatory))),
-  assert(user:file_search_path(su,     project(semantic_uris  ))),
-  assert(user:file_search_path(stcn,   project(stcn           ))),
-  assert(user:file_search_path(swag,   project(swag           ))),
-  assert(user:file_search_path(webqr,  project(webqr          ))).
+index(Dir):-
+  assert(user:file_search_path(prasem, Dir)),
+    assert(user:file_search_path(bk,     prasem(beekeeper      ))),
+    assert(user:file_search_path(ckan,   prasem(ckan           ))),
+    assert(user:file_search_path(dh,     prasem(data_hives     ))),
+    assert(user:file_search_path(el  ,   prasem(energy_labels  ))),
+    assert(user:file_search_path(humr,   prasem(humr           ))),
+    assert(user:file_search_path(id,     prasem(ideaology      ))),
+    assert(user:file_search_path(iotw,   prasem(iotw           ))),
+    assert(user:file_search_path(lodobs, prasem(lod_observatory))),
+    assert(user:file_search_path(su,     prasem(semantic_uris  ))),
+    assert(user:file_search_path(stcn,   prasem(stcn           ))),
+    assert(user:file_search_path(swag,   prasem(swag           ))),
+    assert(user:file_search_path(webqr,  prasem(webqr          ))).
 
