e543316a12729e7101889ce7a93691c4315dc428
Wouter Beek <me@wouterbeek.com>
2014-04-13 22:36:00 +0200

    plc project name fix.

--- run.pl
+++ run.pl
@@ -9,12 +9,12 @@ run_prasem:-
   % Assert project file search path.
   absolute_file_name('.', ProjectDir, [access(read),file_type(directory)]),
   assert(user:file_search_path(project, ProjectDir)),
-  
+
   ensure_loaded(prolog_repository),
   prolog_repository(local, ProjectDir),
-  
+
   % Load PLC index.
-  directory_file_path(ProjectDir, plc, PlcDir),
+  directory_file_path(ProjectDir, 'Prolog-Library-Collection', PlcDir),
   absolute_file_name(
     index,
     PlcIndexFile,
@@ -25,7 +25,7 @@ run_prasem:-
     index(PlcDir),
     unload_file(PlcIndexFile)
   ),
-  
+
   % Load PraSem.
   use_module(prasem(prasem_clas)).
 
