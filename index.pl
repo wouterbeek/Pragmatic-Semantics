b966459b220893b413efedb04544f496e6e9782f
Wouter Beek <me@wouterbeek.com>
2014-04-13 15:46:51 +0200

    Make sure index dirs are created.

--- index.pl
+++ index.pl
@@ -1,17 +1,29 @@
 % Index for the Pragmatic Semantics repository.
 
 index(PrasemDir):-
-  assert(user:file_search_path(prasem, PrasemDir)),
-    assert(user:file_search_path(bk,     prasem(beekeeper      ))),
-    assert(user:file_search_path(ckan,   prasem(ckan           ))),
-    assert(user:file_search_path(dh,     prasem(data_hives     ))),
-    assert(user:file_search_path(el  ,   prasem(energy_labels  ))),
-    assert(user:file_search_path(humr,   prasem(humr           ))),
-    assert(user:file_search_path(id,     prasem(ideaology      ))),
-    assert(user:file_search_path(iotw,   prasem(iotw           ))),
-    assert(user:file_search_path(lodobs, prasem(lod_observatory))),
-    assert(user:file_search_path(su,     prasem(semantic_uris  ))),
-    assert(user:file_search_path(stcn,   prasem(stcn           ))),
-    assert(user:file_search_path(swag,   prasem(swag           ))),
-    assert(user:file_search_path(webqr,  prasem(webqr          ))).
+  assert_index(prasem, PrasemDir)),
+    assert_index(bk,     prasem(beekeeper)),
+    assert_index(ckan,   prasem(ckan)),
+    assert_index(dh,     prasem(data_hives)),
+    assert_index(el  ,   prasem(energy_labels)),
+    assert_index(humr,   prasem(humr)),
+    assert_index(id,     prasem(ideaology)),
+    assert_index(iotw,   prasem(iotw)),
+    assert_index(lodobs, prasem(lod_observatory)),
+    assert_index(su,     prasem(semantic_uris)),
+    assert_index(stcn,   prasem(stcn)),
+    assert_index(swag,   prasem(swag)),
+    assert_index(webqr,  prasem(webqr)).
+
+
+assert_index(Alias, Path):-
+  is_absolute_file_name(Path), !,
+  make_directory_path(Path),
+  assert(user:file_searhc_path(Alias, Path)).
+assert_index(Alias, Path):-
+  Path =.. [Parent(Child)],
+  absolute_file_name(Parent, ParentDir, [file_type(directory)]),
+  directory_file_path(ParentDir, Child, ChildDir),
+  make_directory_path(ChildDir),
+  assert(user:file_search_path(Alias, ChildDir)).
 
