dea9a0774200c54b194ca6874ca0c8bc5faa06f4
Wouter Beek <me@wouterbeek.com>
2014-04-14 15:36:29 +0200

    * [DEP] Special execution of command-line options, supporting PraSem projects.

--- run.pl
+++ run.pl
@@ -6,6 +6,7 @@
 :- set_prolog_flag(verbose_load, silent).
 
 :- use_module(library(filesex)).
+:- use_module(library(option)).
 
 :- multifile(user:project/2).
 user:project('PraSem', 'Pragmatic Semantics for the Web of Data').
@@ -18,11 +19,11 @@ run_prasem:-
   file_directory_name(ThisFile, ThisDir),
   assert(user:file_search_path(project, ThisDir)),
   assert(user:file_search_path(prasem, ThisDir)),
-  
+
   % Prolog repository.
   ensure_loaded(prolog_repository),
   prolog_repository(local, ThisDir),
-  
+
   % The PLC is available as a Git submodule.
   directory_file_path(ThisDir, 'Prolog-Library-Collection', PlcDir),
   absolute_file_name(
@@ -35,7 +36,24 @@ run_prasem:-
     assert_index(PlcDir),
     unload_file(PlcIndexFile)
   ),
-  
+
   % Process the `project` command-line option.
   ensure_loaded(prasem_clas),
-  use_module(pl(pl_clas)).
+  use_module(pl(pl_clas)),
+  gtrace,
+  prasem_process_options.
+
+prasem_process_options:-
+  read_options(O1),
+  prasem_process_options(O1).
+
+prasem_process_options(O1):-
+  select_option(data(Data), O1, O2), !,
+  user:process_option(data(Data)),
+  prasem_process_options(O2).
+prasem_process_options(O1):-
+  select_option(project(Project), O1, O2), !,
+  user:process_option(project(Project)),
+  prasem_process_options(O2).
+prasem_process_options(O1):-
+  process_options(O1).
